622beba1cef5aba8e4921f1a45b997c07db2ab63
HuyHuynh <huy.huynh@titancorpvn.com>
2018-02-22 18:47:19 +0700

    done for case hard k=3, n=5

--- AlgoDemo/src/main/java/Listed.java
+++ AlgoDemo/src/main/java/Listed.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Created by huy.huynh on 08/02/2018.
@@ -10,10 +10,17 @@ public class Listed {
 
         printArray(getFirstArray(k));
         printArray(getLastArray(k, n));
+        System.out.println("");
+
+        //        printArray(getMinInPosition(k));
+        //        printArray(getMaxInPosition(k, n));
+
         int[] previousArray = null;
         do {
             previousArray = getNextArray(previousArray, k, n);
-            printArray(previousArray);
+            if (previousArray != null && checkDuplicateByAlgorithm(previousArray))
+                printArray(previousArray);
+
         } while (previousArray != null);
     }
 
@@ -28,29 +35,47 @@ public class Listed {
     }
 
     private int[] buildNextArray(int[] previousArray, int k, int n) {
-        int max0 = 3;
-        int max1 = 4;
-        int max2 = 5;
-        int min0 = 1;
-        int min1 = 2;
-        int min2 = 3;
+        int[] maxInPosition = getLastArray(k, n);
+        int[] minInPosition = getFirstArray(k);
 
         //        for (int i = k - 1; i > 0; i--) {
-        if (previousArray[k - 1] < max2) {
+        if (previousArray[k - 1] < maxInPosition[k - 1]) {
             previousArray[k - 1]++;
             return previousArray;
-        } else if (previousArray[k - 2] < max1) {
+        } else if (previousArray[k - 2] < maxInPosition[k - 2]) {
             previousArray[k - 2]++;
-            previousArray[k - 1] = min2;
+            previousArray[k - 1] = minInPosition[k - 1];
             return previousArray;
         } else {
             previousArray[k - 3]++;
-            previousArray[k - 2] = min1;
-            previousArray[k - 1] = min2;
+            previousArray[k - 2] = minInPosition[k - 2];
+            previousArray[k - 1] = minInPosition[k - 1];
             return previousArray;
         }
+
     }
-    //    }
+
+    private boolean checkDuplicateByAlgorithm(int[] array) {
+        for (int j = 0; j < array.length; j++)
+            for (int k = j + 1; k < array.length; k++)
+                if (k != j && array[k] <= array[j])
+                    return false;
+        return true;
+    }
+
+    private boolean checkDuplicateBySet(int[] array) {
+        //        Integer[] newArray= array;
+
+        Integer[] result = new Integer[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = Integer.valueOf(array[i]);
+        }
+        Set<Integer> set = new HashSet(Arrays.asList(result));
+        if (set.size() == array.length) {
+            return true;
+        }
+        return false;
+    }
 
     private int[] getFirstArray(int k) {
         int first[] = new int[k];
@@ -68,8 +93,26 @@ public class Listed {
         return last;
     }
 
+    //    private int[] getMinInPosition(int k) {
+    //        int min[] = new int[k];
+    //        for (int i = 0; i < k; i++) {
+    //            min[i] = i + 1;
+    //        }
+    //        return min;
+    //    }
+    //
+    //    private int[] getMaxInPosition(int k, int n) {
+    //        int max[] = new int[k];
+    //        for (int i = 0; i < k; i++) {
+    //            max[i] = n + (i - k) + 1;
+    //        }
+    //        return max;
+    //    }
+
     private void printArray(int[] array) {
         //        System.out.println(String.format("{%s, %s, %s}", array[0], array[1], array[2]));
+        if (array == null)
+            return;
         String printBuffer = "{";
         for (int i = 0; i <= array.length; i++) {
             if (i != array.length) {
